4cdc6501fab96d2fb1bcf454c176b2f1ec44f871
MarkizOrias <georgi.bargan@gmail.com>
2022-12-18 00:50:38 +0100

    up

---
+++ far_cel_converter/target/debug/deps/far_cel_converter-3d8420381ff73ff9.d
@@ -0,0 +1,5 @@
+c:\Users\Georgi\RUST\far_cel_converter\target\debug\deps\far_cel_converter-3d8420381ff73ff9.rmeta: src\main.rs
+
+c:\Users\Georgi\RUST\far_cel_converter\target\debug\deps\far_cel_converter-3d8420381ff73ff9.d: src\main.rs
+
+src\main.rs:
---
+++ far_cel_converter/target/debug/deps/far_cel_converter-d0a9e760c4e25d7e.d
@@ -0,0 +1,5 @@
+c:\Users\Georgi\RUST\far_cel_converter\target\debug\deps\far_cel_converter-d0a9e760c4e25d7e.rmeta: src\main.rs
+
+c:\Users\Georgi\RUST\far_cel_converter\target\debug\deps\far_cel_converter-d0a9e760c4e25d7e.d: src\main.rs
+
+src\main.rs:
