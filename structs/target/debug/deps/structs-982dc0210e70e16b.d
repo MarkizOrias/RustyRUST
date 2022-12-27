162aceaf3734e2ff283ac70ae8f8670df3e67205
MarkizOrias <georgi.bargan@gmail.com>
2022-12-27 05:51:05 +0100

    initiated structs

---
+++ structs/target/debug/deps/structs-982dc0210e70e16b.d
@@ -0,0 +1,5 @@
+c:\Users\Georgi\RUST\structs\target\debug\deps\structs-982dc0210e70e16b.rmeta: src\main.rs
+
+c:\Users\Georgi\RUST\structs\target\debug\deps\structs-982dc0210e70e16b.d: src\main.rs
+
+src\main.rs:
---
+++ structs/target/debug/deps/structs-cc2c1691abe7336a.d
@@ -0,0 +1,5 @@
+c:\Users\Georgi\RUST\structs\target\debug\deps\structs-cc2c1691abe7336a.rmeta: src\main.rs
+
+c:\Users\Georgi\RUST\structs\target\debug\deps\structs-cc2c1691abe7336a.d: src\main.rs
+
+src\main.rs:
